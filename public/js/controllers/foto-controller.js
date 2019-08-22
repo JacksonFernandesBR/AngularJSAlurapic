39c2d30b3cab1ddb14006f5f32c22616866a13fd
Jackson Santos <oiitau@live.com>
2019-08-22 15:32:30 -0300

    Feito até a parte 10

--- public/js/controllers/foto-controller.js
+++ public/js/controllers/foto-controller.js
@@ -1,9 +1,69 @@
 angular.module('alurapic')
-.controller('FotoController', function($scope){
-    
-    $scope.foto = {};
+    .controller('FotoController', function($scope, recursoFoto, $routeParams) {
 
-    $scope.submeter = function(){
-        console.log($scope.foto);
-    };
-});
+        $scope.foto = {};
+        
+        if($routeParams.fotoId){
+            /*$http.get('/v1/fotos/' + $routeParams.fotoId)
+            .success(function(foto){
+                $scope.foto = foto;
+            })
+            .error(function(erro){
+                $scope.mensagem = 'Não foi possível obter a foto'
+            });*/
+
+            recursoFoto.get({fotoId: $routeParams.fotoId}, function(foto){
+                $scope.foto = foto;
+            }, function(erro){
+                console.log(erro);
+                $scope.mensagem = 'Não foi possível obter a foto'
+            });
+        }
+
+        $scope.submeter = function() {
+
+            if ($scope.formulario.$valid) {
+
+                if($routeParams.fotoId){
+                    /*$http.put('/v1/fotos/'+ $scope.foto._id, $scope.foto)
+                    .sucess(function(){
+                        $scope.mensagem = ' Foto ' + $scope.foto.titulo + ' foi alterarada';
+                    })
+                    .error(function(erro){
+                        console.log(erro);
+                        $scope.mensagem = 'Não foi possível alterar';
+                    });*/
+
+                    recursoFoto.update({fotoId: $scope.foto._id},
+                        $scope.foto, function(){
+                            $scope.mensagem = 'Foto alterada com sucesso';
+                        }, function(erro){
+                            console.log(erro);
+                            $scope.mensagem = 'Não foi possível alterar';
+                        });
+                }else{
+                    recursoFoto.save($scope.foto, function(){
+                        $scope.foto = {};
+                        $scope.mensagem = 'Foto cadastrada com sucesso';                      
+                    }, function(erro){
+                        $scope.mensagem = 'Não foi possível cadastrar a foto';
+                    });
+                    
+                    
+                    
+                    /*$http.post('/v1/fotos', $scope.foto)
+                    .success(function() {
+                        console.log('Foto adicionada com sucesso');
+                    })
+                    .error(function(erro) {
+                        console.log('Não foi possível cadastra a foto');
+                    })*/
+
+
+                }
+
+                
+            }
+        };
+
+    });
--- public/js/controllers/fotos-controler.js
+++ public/js/controllers/fotos-controler.js
@@ -1,31 +1,62 @@
-angular.module('alurapic').controller('FotosController', function($scope, $http){
- $scope.fotos = [];
- $scope.filtro = '';
- $http.get('/v1/fotos')
-    .success(function(retorno){
-        console.log(retorno);
-        $scope.fotos = retorno;
-    })
-    .error(function(erro){
-        console.log = erro;
-    });//Código que busca dados no servidor.
+angular.module('alurapic').controller('FotosController', function ($scope, recursoFoto) {
+    
+    $scope.fotos = [];
+    $scope.filtro = '';
+    $scope.mensagem = '';
+    recursoFoto.query(function(fotos){
+        $scope.fotos = fotos;
+    }, function(erro){
+        console.log(erro);
+    });
 
+    //    $html.get('/v1/fotos/')
+      //  .success(function (retorno) {
+        //    console.log(retorno);
+          //  $scope.fotos = retorno;
+       // })
+       // .error(function (erro) {
+       //     console.log = erro;
+       // });//Código que busca dados no servidor.
+
+    $scope.remover = function (foto) {
+        recursoFoto.delete({fotoId: foto._id}, function(){
+            var indiceDaFoto = $scope.fotos.indexOf(foto);
+            $scope.fotos.splice(indiceDaFoto, 1);
+            $scope.mensagem = 'Foto' + foto.titulo + 'removida com sucesso!';
+        },function(erro){
+            console.log(erro);
+            $scope.mensagem = 'Não foi possível apagar a foto' + foto.titulo;
+        });
+
+        
+/*        $http.delete('/v1/fotos/' + foto._id)
+            .success(function () {
+                var indiceDaFoto = $scope.fotos.indexOf(foto);
+                $scope.fotos.splice(indiceDaFoto, 1);
+                $scope.mensagem = 'Foto ' + foto.titulo + ' Removida com sucesso!';
+            })
+            .error(function (erro) {
+                console.log(erro);
+                console.mensagem = 'Não foi possível apagar a foto' + foto.titulo;
+
+            });*/
+    };
 });
 
-    /*Código utilizado quando não se buscava fotos do servidor.
- $scope.fotos = [
-     {
-    url : 'https://ap.imagensbrasil.org/images/imagens-lobos.jpg',
-    titulo : 'Lobo1'
-    },
+/*Código utilizado quando não se buscava fotos do servidor.
+$scope.fotos = [
+ {
+url : 'https://ap.imagensbrasil.org/images/imagens-lobos.jpg',
+titulo : 'Lobo1'
+},
 
-    {
-    url : 'https://ap.imagensbrasil.org/images/imagens-lobos.jpg',
-    titulo : 'Lobo2'
-    },
+{
+url : 'https://ap.imagensbrasil.org/images/imagens-lobos.jpg',
+titulo : 'Lobo2'
+},
 
-    {
-    url : 'https://ap.imagensbrasil.org/images/imagens-lobos.jpg',
-    titulo : 'Lobo3'
-    }
+{
+url : 'https://ap.imagensbrasil.org/images/imagens-lobos.jpg',
+titulo : 'Lobo3'
+}
 ];*/
